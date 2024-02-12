1afd194de99885106bae89be20318ea57479d4a4
lunareraze <sapitridina88@gmail.com>
2024-02-12 15:58:59 +0700

    chore: initialized new pages

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:dina_sm/home/home.view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const HomeView(),
+    );
+  }
+}
