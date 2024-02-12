080cf85e52824cfc296dfb076886c180327faf1a
lunareraze <sapitridina88@gmail.com>
2024-02-12 22:55:41 +0700

    feature: reative stateless widget

--- lib/home/home.view.dart
+++ lib/home/home.view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:states_rebuilder/scr/state_management/extensions/type_extensions.dart';
+import 'package:states_rebuilder/scr/state_management/rm.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends ReactiveStatelessWidget {
+  HomeView({super.key});
+
+  final count = 0.inj();
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +13,24 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
         backgroundColor: Colors.grey,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              count.state.toString(),
+              textScaler: const TextScaler.linear(1.5),
+            ),
+            const SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () {
+                count.state = count.state + 3;
+              },
+              child: const Text('Tambah'),
+            )
+          ],
+        ),
       ),
     );
   }
