e8a5860805ce6f6d1ca2f43ab8644d046e93f47d
lunareraze <sapitridina88@gmail.com>
2024-02-12 22:56:11 +0700

    feature: create home data and ctrl page

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const HomeView(),
+      home: HomeView(),
     );
   }
 }
